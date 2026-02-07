fd0db1cb27d8ab20c15ed1164ce52e4c9a0aa4af
zaustrager <zacharyaustrager@gmail.com>
2026-02-07 16:39:26 -0500

    Changes from WordPress Playground -Includes new search function + template

---
+++ patterns/header.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: header
+ * Slug: waterville/header
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"bottom":"0px","top":"30px","left":"var:preset|spacing|60","right":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignfull" style="padding-top:30px;padding-right:0;padding-bottom:0px;padding-left:var(--wp--preset--spacing--60)"><!-- wp:site-title {"style":{"typography":{"fontSize":"22px"}}} /-->
+
+<!-- wp:navigation {"ref":4,"style":{"spacing":{"blockGap":"0"}},"fontFamily":"inter","layout":{"type":"flex","justifyContent":"right"}} /-->
+
+<!-- wp:search {"label":"<?php esc_attr_e('Search', 'waterville');?>","buttonText":"<?php esc_attr_e('Search', 'waterville');?>","style":{"border":{"radius":{"topLeft":"46px","topRight":"46px","bottomLeft":"46px","bottomRight":"46px"}},"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}},"fontFamily":"inter"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
