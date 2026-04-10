37cc40cd26d024d5fc36c221552677d955a15130
zaustrager <zacharyaustrager@gmail.com>
2026-04-10 16:42:06 -0400

    update drop-cap function draft

--- functions.php
+++ functions.php
@@ -5,3 +5,8 @@ function waterville() {
     add_editor_style( 'editor-style.css' );
 }
 add_action( 'after_setup_theme', 'waterville' );
+
+function waterville_scripts() {
+    wp_enqueue_style( 'waterville-style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'waterville_scripts' );
